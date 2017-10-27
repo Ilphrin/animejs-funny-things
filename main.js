e97d39734aa431c732aa43c19e10cd77dee0d982
Kevin Ilphrin Pellet <ilphrin@autistici.org>
2017-10-26 22:44:26 -0400

    LeftBar finished

--- main.js
+++ main.js
@@ -24,10 +24,25 @@ tlLeft.add({
   begin: function() {
     anime({
       targets: spans[1],
-      scale: [1, 2],
-      rotate: ['0deg', '120deg'],
-      direction: 'alternate',
-      duration: 1000,
+      scale: [
+        { value: .5, duration: 0 },
+        { value: 1, duration: 500, delay: 1000 },
+        { value: 2, duration: 500 },
+      ],
+      rotate: [
+        { value: '0deg', duration: 0, elasticity: 0 },
+        { value: '120deg', duration: 500, elasticity: 0 },
+        { value: '115deg', duration: 150, delay: 500 },
+        { value: '65deg', duration: 400, delay: 400 },
+      ],
+      left: [
+        { value: '50%', duration: 0, delay: 0, elasticity: 0 },
+        { value: '45%', duration: 500, elasticity: 0 },
+        { value: '49.5%', duration: 500, delay: 500 , elasticity: 0},
+        { value: '55%', duration: 500},
+      ],
+      easing: 'easeInOutQuad',
     })
   }
-})
+});
+
