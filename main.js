34858241b7e3576b7d9c484b4cd87f4942feddc4
Kevin Ilphrin Pellet <ilphrin@autistici.org>
2017-10-28 02:01:53 -0400

    Finished animation, looks like a nice menu hamburger, no?

--- main.js
+++ main.js
@@ -8,12 +8,17 @@ let tlLeft = anime.timeline({
   complete: function() { tlLeft.restart(); },
 });
 
+let tlRight = anime.timeline({
+  duration: 10000,
+  complete: function() { tlRight.restart(); },
+});
+
 tlBottom.add({
   begin: function() {
     anime({
       targets: spans[0],
       scale: [1, 4],
-      top: ['50%', '70%'],
+      top: ['50%', '60%'],
       duration: 1000,
       easing: 'easeInOutQuad',
     });
@@ -25,20 +30,29 @@ tlLeft.add({
     anime({
       targets: spans[1],
       scale: [
-        { value: .5, duration: 0 },
-        { value: 1, duration: 500, delay: 1000 },
+        { value: .5, duration: 500},
+        { value: 1, duration: 500},
         { value: 2, duration: 500 },
+        { value: 4, duration: 500, delay: 500 },
       ],
       rotate: [
-        { value: '0deg', duration: 0, elasticity: 0 },
-        { value: '120deg', duration: 500, elasticity: 0 },
-        { value: '115deg', duration: 150, delay: 500 },
-        { value: '65deg', duration: 400, delay: 400 },
+        { value: '0deg', duration: 0, elasticity: 0},
+        { value: '120deg', duration: 500, elasticity: 0},
+        { value: '115deg', duration: 400},
+        { value: '65deg', duration: 400},
+        { value: '0deg', duration: 500, delay: 500},
       ],
       left: [
-        { value: '50%', duration: 0, delay: 0, elasticity: 0 },
+        { value: '50%', duration: 0, delay: 0, elasticity: 0},
         { value: '45%', duration: 500, elasticity: 0 },
-        { value: '49.5%', duration: 500, delay: 500 , elasticity: 0},
+        { value: '49.5%', duration: 350, elasticity: 0},
+        { value: '55%', duration: 500, delay: 100},
+        { value: '50%', duration: 500, delay: 500},
+      ],
+      top: [
+        { value: '50%', duration: 0},
+        { value: '50%', duration: 500},
+        { value: '50%', duration: 500, delay: 500},
         { value: '55%', duration: 500},
       ],
       easing: 'easeInOutQuad',
@@ -46,3 +60,37 @@ tlLeft.add({
   }
 });
 
+tlRight.add({
+  begin: function() {
+    anime({
+      targets: spans[2],
+      scale: [
+        { value: .5, duration: 500},
+        { value: 1, duration: 500},
+        { value: 2, duration: 500 },
+        { value: 4, duration: 500, delay: 500 },
+      ],
+      rotate: [
+        { value: '0deg', duration: 0, elasticity: 0},
+        { value: '65deg', duration: 500},
+        { value: '65deg', duration: 400},
+        { value: '120deg', duration: 400},
+        { value: '0deg', duration: 500, delay: 500},
+      ],
+      left: [
+        { value: '50%', duration: 0, delay: 0, elasticity: 0},
+        { value: '55%', duration: 500, delay: 100},
+        { value: '50.5%', duration: 350, elasticity: 0},
+        { value: '45%', duration: 500, elasticity: 0 },
+        { value: '50%', duration: 500, delay: 500},
+      ],
+      top: [
+        { value: '50%', duration: 0},
+        { value: '50%', duration: 500},
+        { value: '50%', duration: 500, delay: 500},
+        { value: '50%', duration: 500},
+      ],
+      easing: 'easeInOutQuad',
+    })
+  }
+})
